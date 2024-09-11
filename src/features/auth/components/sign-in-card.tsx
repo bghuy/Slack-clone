a71424fb7412eaddf1ad7fe6c4287a667dd89df5
Bạch Gia Huy <bghuy2003@gmail.com>
2024-09-12 00:26:53 +0700

    set up

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card"
+
+export const SignInCard = () =>{
+    return (
+        <Card className="w-full h-full p-8">
+            Sign In Card
+        </Card>
+    )
+}
