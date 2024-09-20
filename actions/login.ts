1ed0c25c84f923d5d99b01596d8ceb44b41cb509
Bạch Gia Huy <bghuy2003@gmail.com>
2024-09-21 00:13:05 +0700

    update prisma and add sub to session

--- actions/login.ts
+++ actions/login.ts
@@ -1,17 +1,35 @@
 'use server'
 import * as z from "zod"
 import { LoginSchema } from "@/schemas";
-type LoginResponse = { success?: string; error?: string };
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
-export const login = async(values: z.infer<typeof LoginSchema>): Promise<LoginResponse> => {
-    return new Promise((resolve, reject) => {
-        const validatedFields = LoginSchema.safeParse(values);
-        setTimeout(() => {
-            if (!validatedFields.success) {
-                reject({ error: "Login failed!" });
-            } else {
-                resolve({ success: "Login successfully!" });
+export const login = async(values: z.infer<typeof LoginSchema>) => {
+
+    const validatedFields = LoginSchema.safeParse(values);
+    if (!validatedFields.success) {
+        return({ error: "Login failed!" });
+    }
+    const {email , password } = validatedFields.data;
+    try {
+        await signIn("credentials",{
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT
+        })
+        return({ success: "Login successfully!" });
+    } catch (error) {
+        if(error instanceof AuthError){
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid credentials!"}
+                default:
+                    return { error: "Something went wrong!"}
             }
-        }, 1000);
-    });
+        }
+        throw error;
+    }
+        
+
 }
