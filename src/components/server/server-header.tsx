3cab59031cc416d322f88f631ee5ddb018baf759
Bạch Gia Huy <bghuy2003@gmail.com>
2024-09-29 00:32:46 +0700

    add invite code api

--- src/components/server/server-header.tsx
+++ src/components/server/server-header.tsx
@@ -4,6 +4,7 @@ import { ServerWithMembersWithUsers } from "@/types/types"
 import { MemberRole } from "@prisma/client";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import { useModal } from "../../../hooks/use-modal-store";
 
 interface ServerHeaderProps {
     server: ServerWithMembersWithUsers;
@@ -15,6 +16,7 @@ export const ServerHeader = ({
     server,
     role
 }: ServerHeaderProps) =>{
+    const {onOpen} = useModal();
     const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR;
     return(
@@ -36,6 +38,7 @@ export const ServerHeader = ({
                 {
                     isModerator && (
                         <DropdownMenuItem
+                            onClick={()=> onOpen("invite", {server})}
                             className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
                         >
                             Invite People
